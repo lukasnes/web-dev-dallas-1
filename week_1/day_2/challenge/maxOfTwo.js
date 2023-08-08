c86b5108ac0859373a25a0b44da7ffd3ca48be50
Parker Sheehan <Parkersheehan007@gmail.com>
2023-08-08 14:24:55 -0500

    maxOfTwo added

---
+++ week_1/day_2/challenge/maxOfTwo.js
@@ -0,0 +1,12 @@
+let x = 8
+let y = 2
+
+if (x > y) {
+    console.log(`${x} is larger than ${y}`)
+} 
+else if (y > x){
+    console.log(y + "is larger than" + x)
+}
+else{
+    console.log("they are equal")
+}
